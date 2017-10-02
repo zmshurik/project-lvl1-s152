1b6c4d3aa682067ce7f5abaca6963f0033763c3b
Alexander Kurakov <zmshurik@yandex.ru>
2017-10-02 16:21:23 +0700

    create cli.php
    add run function with request for name of player.

---
+++ src/Cli.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BraiGames\Cli;
+
+use function \cli\line;
+use function \cli\prompt;
+
+
+function run()
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
