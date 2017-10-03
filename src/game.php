1c4786868517d1d7d69817a96e24a3328176033c
Alexander Kurakov <zmshurik@yandex.ru>
2017-10-03 22:07:27 +0700

    change game.php
    delete unnecessary checks

--- src/game.php
+++ src/game.php
@@ -17,33 +17,30 @@ function play($gameName)
         case 'BrainCalc':
             $game = BrainCalc();
     }
-    if (isset($game)) {
-        line($game('getGameDescription'));
-    }
+    line($game('getGameDescription'));
     line();
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line();
-    if (isset($game)) {
-        $questions = $game('getQuestions');
-        $correctAnswers = $game('getAnswers');
-        $result = array_reduce($questions, function ($step, $question) use ($correctAnswers, $game) {
-            if ($step) {
-                $correctAnswer = $correctAnswers[$step - 1];
-                line("Question: $question");
-                $answer = prompt('Your answer');
-                if ($game('isCorrect', $answer, $correctAnswer)) {
-                    line('Correct!');
-                    return $step + 1;
-                }
-                line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
-                return false;
+    
+    $questions = $game('getQuestions');
+    $correctAnswers = $game('getAnswers');
+    $result = array_reduce($questions, function ($step, $question) use ($correctAnswers, $game) {
+        if ($step) {
+            $correctAnswer = $correctAnswers[$step - 1];
+            line("Question: $question");
+            $answer = prompt('Your answer');
+            if ($game('isCorrect', $answer, $correctAnswer)) {
+                line('Correct!');
+                return $step + 1;
             }
-            return $step;
-        }, 1);
-        $message = $result ? 'Congratulations, %s!' : 'Let\'s try again, %s!';
-        line($message, $name);
-    }
+            line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
+            return false;
+        }
+        return $step;
+    }, 1);
+    $message = $result ? 'Congratulations, %s!' : 'Let\'s try again, %s!';
+    line($message, $name);
 }
 
 function runBrainEven()
