17dee9b4e719ca4f6193e89db193fd60504a488e
Alexander Kurakov <zmshurik@yandex.ru>
2017-10-04 21:07:42 +0700

    change gameEngine

--- src/GameEngine.php
+++ src/GameEngine.php
@@ -5,16 +5,21 @@ namespace BrainGames\GameEngine;
 use function \cli\line;
 use function \cli\prompt;
 
-function step($gameData)
+define('STEP_AMOUNT', 3);
+
+function gameProcess($game)
 {
-    list($question, $rightAnswer) = $gameData;
-    line("Question: " . $question);
-    $answer = prompt('Your answer');
-    if ($answer != $rightAnswer) {
-        line("'$answer' is wrong answer ;(. Correct answer was '$rightAnswer'.");
-        return false;
+    for ($step = 1; $step <= STEP_AMOUNT; $step++) {
+        $gameData = $game();
+        list($question, $rightAnswer) = $gameData;
+        line("Question: " . $question);
+        $answer = prompt('Your answer');
+        if ($answer != $rightAnswer) {
+            line("'$answer' is wrong answer ;(. Correct answer was '$rightAnswer'.");
+            return false;
+        }
+        line('Correct!');
     }
-    line('Correct!');
     return true;
 }
 
@@ -26,14 +31,7 @@ function play($game, $description)
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line();
-    define('STEP_AMOUNT', 3);
-    for ($step = 1; $step <= STEP_AMOUNT; $step++) {
-        $gameData = $game();
-        $isWin = step($gameData);
-        if (!$isWin) {
-            break;
-        }
-    }
+    $isWin = gameProcess($game);
     $message = $isWin ? 'Congratulations, %s!' : 'Let\'s try again, %s!';
     line($message, $name);
 }
