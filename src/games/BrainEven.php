b51d984b74d4c687595918b3cb6663403280b3cb
Alexander Kurakov <zmshurik@yandex.ru>
2017-10-05 20:03:33 +0700

    fix BrainEven

--- src/games/BrainEven.php
+++ src/games/BrainEven.php
@@ -6,17 +6,15 @@ use function \BrainGames\GameEngine\play;
 
 define('DESCRIPTION_OF_BRAIN_EVEN', 'Answer "yes" if number even otherwise answer "no".');
 
-
-
 function playGame()
 {
-    $getGameData = function () {
-        $isEven = function ($number) {
-            return $number % 2 == 0;
-        };
-        $getAnswer = function ($question) use ($isEven) {
-            return $isEven($question) ? 'yes' : 'no';
-        };
+    $isEven = function ($number) {
+        return $number % 2 == 0;
+    };
+    $getAnswer = function ($question) use ($isEven) {
+        return $isEven($question) ? 'yes' : 'no';
+    };
+    $getGameData = function () use ($getAnswer) {
         $question = \rand(1, 100);
         return [$question, $getAnswer($question)];
     };
