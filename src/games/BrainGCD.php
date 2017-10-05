18954d76dcaefdedba815820d36aa6c2909efe0c
Alexander Kurakov <zmshurik@yandex.ru>
2017-10-05 15:18:53 +0700

    add brain-gcd

---
+++ src/games/BrainGCD.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace BrainGames\Games\BrainGCD;
+
+use function \BrainGames\GameEngine\play;
+
+define('DESCRIPTION_OF_BRAIN_GCD', 'Find the greatest common divisor of given numbers.');
+
+function playGame()
+{
+    $getGameData = function () {
+        $gcd = function ($a, $b) use (&$gcd) {
+            return $b ? $gcd($b, $a % $b) : $a;
+        };
+        $number1 = random_int(1, 100);
+        $number2 = random_int(1, 100);
+        return ["$number1 $number2", $gcd($number1, $number2)];
+    };
+    play($getGameData, DESCRIPTION_OF_BRAIN_GCD);
+}
