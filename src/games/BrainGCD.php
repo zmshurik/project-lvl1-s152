5b7f9a6791de6e710152ce5130495213b0fd803f
Alexander Kurakov <zmshurik@yandex.ru>
2017-10-05 15:56:56 +0700

    change BrainGCD

--- src/games/BrainGCD.php
+++ src/games/BrainGCD.php
@@ -8,10 +8,10 @@ define('DESCRIPTION_OF_BRAIN_GCD', 'Find the greatest common divisor of given nu
 
 function playGame()
 {
-    $getGameData = function () {
-        $gcd = function ($a, $b) use (&$gcd) {
-            return $b ? $gcd($b, $a % $b) : $a;
-        };
+    $gcd = function ($a, $b) use (&$gcd) {
+        return $b ? $gcd($b, $a % $b) : $a;
+    };
+    $getGameData = function () use ($gcd) {
         $number1 = random_int(1, 100);
         $number2 = random_int(1, 100);
         return ["$number1 $number2", $gcd($number1, $number2)];
